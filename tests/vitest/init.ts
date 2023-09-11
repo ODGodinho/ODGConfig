0b90a408b0c39188ba3f879702e95087a0b1f606
Victor Godinho <DragonsGamers@hotmail.com.br>
2023-09-10 21:33:19 -0300

    Initial commit

---
+++ tests/vitest/init.ts
@@ -0,0 +1,5 @@
+import "vitest/globals.d.ts";
+
+export default void (async (): Promise<void> => {
+    // Test init
+})();
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+const vite = defineConfig({
+    test: {
+        globals: true,
+        coverage: {
+            enabled: true,
+            provider: "v8",
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+            exclude: [
+                "src/index.ts",
+                "src/index.js",
+            ],
+        },
+        setupFiles: [
+            "./tests/vitest/init.ts",
+        ],
+    },
+    plugins: [ tsconfigPaths() ],
+
+});
+
+export default vite;
